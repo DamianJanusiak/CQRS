17b4c22d0dfaa204a4d09919464f0a32e1cfd6fc
Damian Janusiak <Damian.Janusiak@ebicom.pl>
2025-01-20 12:30:25 +0100

    notifcations

---
+++ Aplication/Notifications/Handlers/NotificationHandlerExample2.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CQRS.Notifications.Handlers
+{
+    public class NotificationHandlerExample2 : INotificationHandler<Notification>
+    {
+        public Task Handle(Notification notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Notification 2 Example: {notification.Txt}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
